31d48c25b012d3c30d9822e815a6bc7c20521d24
Mark Ivchenko <marxygen@gmail.com>
2021-04-11 21:16:32 +0300

    Working on creation process

--- index.py
+++ index.py
@@ -2,6 +2,7 @@ import argparse
 from datetime import datetime as dt
 from misc import create_directory
 from virtenv import create_virtual_env
+from add_git import initialize_repo
 from django_proj import create_django_project
 
 if __name__ != '__main__':
@@ -10,7 +11,7 @@ if __name__ != '__main__':
 
 parser = argparse.ArgumentParser(description="Creates a Django project with virtual environment, Git repository and Docker container")
 parser.add_argument('name', type=str, help="Name of your project")
-parser.add_argument('-r', type=str, help="Link to the repository")
+parser.add_argument('-r', '--repository', type=str, help="Link to the repository")
 args = parser.parse_args()
 
 print(f'[{dt.now()}] Initializing "{args.name}"...')
@@ -18,6 +19,13 @@ create_directory(args.name) # Create directory
 print('\tProject folder created')
 create_virtual_env(args.name) # Create a virtual environment in this directory
 print('\tVirtual environment created')
+
+#create_django_project(args.name)
+#print('\tDjango project created')
+
+# if args.repository:
+#     initialize_repo(args.repository)
+#     print('Git repository initialized')
 
 
 

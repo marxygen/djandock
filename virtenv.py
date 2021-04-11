7a49e847d7f011e0d568b70ad90e7d723ce8cbe0
Mark Ivchenko <marxygen@gmail.com>
2021-04-11 18:29:20 +0300

    Now the packages will be updated

--- virtenv.py
+++ virtenv.py
@@ -2,5 +2,5 @@ import venv, os
 from presets import VIRTUALENV_DIR
 
 def create_virtual_env(directory):
-    builder = venv.EnvBuilder(with_pip=True)
+    builder = venv.EnvBuilder(with_pip=True, upgrade_deps=True)
     builder.create(os.path.join(os.getcwd(), directory, VIRTUALENV_DIR))

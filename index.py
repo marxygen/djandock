96facbb8884a59f63c37d87eafc7ebd3026699f1
Mark Ivchenko <marxygen@gmail.com>
2021-04-11 21:54:07 +0300

    Docker file processing added

--- index.py
+++ index.py
@@ -4,6 +4,7 @@ from misc import create_directory
 from virtenv import create_virtual_env
 from add_git import initialize_repo
 from django_proj import create_django_project
+from docker_files import wrap
 
 if __name__ != '__main__':
     print('This file is to be executed directly')
@@ -21,7 +22,8 @@ create_virtual_env(args.name) # Create a virtual environment in this directory
 print('\tVirtual environment created')
 create_django_project(args.name)
 print('\tDjango project created')
-
+wrap(args.name) # Create Dockerfile and docker-compose.yml file
+print('\tWrapped in Docker')
 # if args.repository:
 #     initialize_repo(args.repository)
 #     print('Git repository initialized')

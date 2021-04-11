b46218b9409e9fdf517c3a582b3957abab62c6b2
Mark Ivchenko <marxygen@gmail.com>
2021-04-11 21:15:52 +0300

    Now virtualenv also installs django

--- virtenv.py
+++ virtenv.py
@@ -1,6 +1,21 @@
 import venv, os
-from presets import VIRTUALENV_DIR
+from presets import VIRTUALENV_DIR, PACKAGE_INSTALL_COMMAND
 
-def create_virtual_env(directory):
+def create_virtual_env(directory, install_libs=True):
     builder = venv.EnvBuilder(with_pip=True, upgrade_deps=True)
     builder.create(os.path.join(os.getcwd(), directory, VIRTUALENV_DIR))
+
+    if install_libs:
+        # Attempt to enter the virtual environment and install required scripts there.
+        # Might be tricky since Windows and Linux differ in activating the virtualenv
+        
+        #code = os.system(PACKAGE_INSTALL_COMMAND.format(name=directory))
+        stream = os.popen(PACKAGE_INSTALL_COMMAND.format(name=directory))
+        output = stream.read()
+        print(output)
+        # if code == 0:
+        #     print('\tDjango installed successfully')
+        #     return
+        # print('\tCouldn\'t install Django. The following steps may fail')
+
+    
